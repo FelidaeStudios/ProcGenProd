8aa9166be600e0e2f320a67ecb5c5b90400fc58a
kennerm1 <123021312+kennerm1@users.noreply.github.com>
2025-03-10 10:41:45 -0700

    arrays

--- procGen/Assets/Scripts/PerlinOctave.cs
+++ procGen/Assets/Scripts/PerlinOctave.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class PerlinOctave : MonoBehaviour
 {
-    float gradient;
+    float gradient[][][2];
     int size;
 
+    private void PerlinOctave(int size)
+    {
+        this.size = size;
 
+        gradient = new float[size + 1][size + 1][2];
+        for (int ix = 0; ix <= size; ix++)
+        {
+            for (int iy = 0; iy <= size; iy++)
+            {
+                gradient[ix][iy][0] = randomRange(-1, 1);
+                gradient[ix][iy][1] = randomRange(-1, 1);
+            }
+        }
+    }
 }
